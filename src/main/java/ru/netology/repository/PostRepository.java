5f3300241da69e075c4477deee0400f6be3c8939
O <otsu.ni@gmail.com>
2021-08-09 21:51:00 +0300

    Jspr-7 lesson 4 homework 1: repository implemented

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,30 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 // Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    private ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
 
-  public Post save(Post post) {
-    return post;
-  }
+    public List<Post> all() {
+        return List.copyOf(posts.values());
+    }
 
-  public void removeById(long id) {
-  }
+    public Optional<Post> getById(long id) {
+        Post post = posts.get(id);
+        return post == null ? Optional.empty() : Optional.of(post);
+    }
+
+    public Post save(Post post) {
+        posts.put(post.getId(), post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
 }
