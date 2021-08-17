ecfa42e8b5c8dd1468331075df48019b3b723fb2
O <otsu.ni@gmail.com>
2021-08-18 00:19:16 +0300

    Jspr-7 lesson 6 homework 1

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,67 +1,46 @@
 package ru.netology.controller;
 
-import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
-
-import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Reader;
+import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
-    public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
 
     public PostController(PostService service) {
         this.service = service;
     }
 
-    public void all(HttpServletResponse response) throws IOException {
-        final var data = service.all();
-        sendResponse(data, response);
+    @GetMapping
+    public List<Post> all() throws IOException {
+        return service.all();
     }
 
-    public void getById(long id, HttpServletResponse response) throws IOException {
-        try {
-            final var data = service.getById(id);
-            sendResponse(data, response);
-        } catch (NotFoundException e) {
-            notFound(response);
-        }
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id) throws IOException {
+        return service.getById(id);
     }
 
-    public void save(Reader body, HttpServletResponse response) throws IOException {
-        final var gson = new Gson();
-        final var post = gson.fromJson(body, Post.class);
-        try {
-            final var data = service.save(post);
-            sendResponse(data, response);
-        } catch (NotFoundException e) {
-            notFound(response);
-        }
+    @PostMapping
+    public Post save(@RequestBody Post post) throws IOException {
+        return service.save(post);
     }
 
-    public void removeById(long id, HttpServletResponse response) throws IOException {
-        try {
-            service.removeById(id);;
-            sendResponse(true, response);
-        } catch (NotFoundException e) {
-            notFound(response);
-        }
-    }
-
-    private void sendResponse(Object data, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var gson = new Gson();
-        response.getWriter().print(gson.toJson(data));
-    }
-
-    private void notFound(HttpServletResponse response) {
-        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    @DeleteMapping("/{id}")
+    public boolean removeById(@PathVariable long id) throws IOException {
+        service.removeById(id);
+        return true;
     }
 }
